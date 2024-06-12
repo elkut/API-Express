ae3aafeaa99af32a90b8f82fc8a416f59313d8f5
elkut <elkut.esker@gmail.com>
2024-06-11 20:06:20 -0400

    Developed UI

--- PostmanCloneUI/Dashboard.Designer.cs
+++ PostmanCloneUI/Dashboard.Designer.cs
@@ -28,20 +28,134 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			callAPi = new Button();
+			header = new Label();
+			urlLable = new Label();
+			resultsBox = new TextBox();
+			statusStrip = new StatusStrip();
+			systemStatus = new ToolStripStatusLabel();
+			resultsLabel = new Label();
+			urlBox = new TextBox();
+			statusStrip.SuspendLayout();
 			SuspendLayout();
+			// 
+			// callAPi
+			// 
+			callAPi.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			callAPi.Location = new Point(660, 105);
+			callAPi.Name = "callAPi";
+			callAPi.Size = new Size(51, 39);
+			callAPi.TabIndex = 0;
+			callAPi.Text = "GO";
+			callAPi.TextAlign = ContentAlignment.TopCenter;
+			callAPi.UseVisualStyleBackColor = true;
+			callAPi.Click += callApi_Click;
+			// 
+			// header
+			// 
+			header.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			header.AutoSize = true;
+			header.Font = new Font("Stencil", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			header.Location = new Point(221, 9);
+			header.Name = "header";
+			header.Size = new Size(345, 57);
+			header.TabIndex = 2;
+			header.Text = "API Checker";
+			header.TextAlign = ContentAlignment.TopCenter;
+			header.Click += label1_Click;
+			// 
+			// urlLable
+			// 
+			urlLable.AutoSize = true;
+			urlLable.Font = new Font("Bernard MT Condensed", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			urlLable.Location = new Point(51, 70);
+			urlLable.Name = "urlLable";
+			urlLable.Size = new Size(52, 28);
+			urlLable.TabIndex = 3;
+			urlLable.Text = "URL:";
+			urlLable.Click += label2_Click;
+			// 
+			// resultsBox
+			// 
+			resultsBox.Location = new Point(51, 210);
+			resultsBox.Multiline = true;
+			resultsBox.Name = "resultsBox";
+			resultsBox.ReadOnly = true;
+			resultsBox.ScrollBars = ScrollBars.Both;
+			resultsBox.Size = new Size(657, 351);
+			resultsBox.TabIndex = 4;
+			resultsBox.TextChanged += textBox1_TextChanged;
+			// 
+			// statusStrip
+			// 
+			statusStrip.Items.AddRange(new ToolStripItem[] { systemStatus });
+			statusStrip.Location = new Point(0, 570);
+			statusStrip.Name = "statusStrip";
+			statusStrip.Size = new Size(773, 27);
+			statusStrip.TabIndex = 6;
+			statusStrip.Text = "statusStrip2";
+			// 
+			// systemStatus
+			// 
+			systemStatus.Font = new Font("Bernard MT Condensed", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			systemStatus.Name = "systemStatus";
+			systemStatus.Size = new Size(60, 22);
+			systemStatus.Text = "Ready";
+			// 
+			// resultsLabel
+			// 
+			resultsLabel.AutoSize = true;
+			resultsLabel.Font = new Font("Bernard MT Condensed", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			resultsLabel.Location = new Point(51, 176);
+			resultsLabel.Name = "resultsLabel";
+			resultsLabel.Size = new Size(83, 28);
+			resultsLabel.TabIndex = 7;
+			resultsLabel.Text = "Results:";
+			resultsLabel.Click += resultsLabel_Click;
+			// 
+			// urlBox
+			// 
+			urlBox.Location = new Point(51, 105);
+			urlBox.Name = "urlBox";
+			urlBox.Size = new Size(602, 39);
+			urlBox.TabIndex = 8;
 			// 
 			// Dashboard
 			// 
 			AutoScaleDimensions = new SizeF(13F, 32F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(1486, 960);
+			AutoSize = true;
+			AutoSizeMode = AutoSizeMode.GrowAndShrink;
+			BackColor = Color.PaleGoldenrod;
+			BackgroundImageLayout = ImageLayout.Center;
+			ClientSize = new Size(773, 597);
+			Controls.Add(urlBox);
+			Controls.Add(resultsLabel);
+			Controls.Add(statusStrip);
+			Controls.Add(resultsBox);
+			Controls.Add(urlLable);
+			Controls.Add(header);
+			Controls.Add(callAPi);
 			Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-			Margin = new Padding(6, 6, 6, 6);
+			ForeColor = Color.Black;
+			Margin = new Padding(6);
 			Name = "Dashboard";
-			Text = "Posman Clone";
+			Text = "API Checker";
+			statusStrip.ResumeLayout(false);
+			statusStrip.PerformLayout();
 			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
+
+		private Button callAPi;
+		private Label header;
+		private Label urlLable;
+		private TextBox resultsBox;
+		private StatusStrip statusStrip;
+		private Label resultsLabel;
+		private TextBox urlBox;
+		private ToolStripStatusLabel systemStatus;
 	}
 }
--- PostmanCloneUI/Dashboard.cs
+++ PostmanCloneUI/Dashboard.cs
@@ -6,5 +6,51 @@ namespace PostmanCloneUI
 		{
 			InitializeComponent();
 		}
+
+		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private async void callApi_Click(object sender, EventArgs e)
+		{
+			// Validate API URL
+
+			try
+			{
+				systemStatus.Text = "Calling API..";
+
+				// Sample code - replace with the actual API call
+				await Task.Delay(2000);
+
+				systemStatus.Text = "Ready";
+			}
+			catch (Exception ex)
+			{
+				resultsBox.Text = "Error: " + ex.Message;
+				systemStatus.Text = "Error";
+			}
+
+		}
+
+		private void resultsLabel_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
