910f362bf9fc3b496f06240d06969f3b6ba1c8fe
elkut <elkut.esker@gmail.com>
2024-07-21 13:52:52 -0400

    some changes

--- PostmanCloneLibrary/IApiAccess.cs
+++ PostmanCloneLibrary/IApiAccess.cs
@@ -4,6 +4,7 @@ namespace PostmanCloneLibrary
     public interface IApiAccess
     {
         Task<string> CallApiAsync(string url, bool formatOutput = true, HttpAction action = HttpAction.GET);
+        Task<string> PostApiAsync(string url, bool formatOutput = true, HttpAction action = HttpAction.POST);
         bool IsValidUrl(string url);
     }
 }
